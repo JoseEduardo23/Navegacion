d769e3f21116db6cdc0535a3911cbc0922f94ee3
Eduardo <joselincango73@gmail.com>
2024-07-11 18:32:33 -0500

    Modificaciones del diseño del Form 1 correspondiente al inicio de sesion

--- Navegacion-Inicio/src/Main.java
+++ Navegacion-Inicio/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // para cerrar el frame
         frame.setContentPane(new Form1().MainPanel1);
         frame.setSize(800, 600);
-        frame.setPreferredSize(new Dimension(1000, 300));
+        frame.setPreferredSize(new Dimension(550, 300));
         frame.pack();
         frame.setLocationRelativeTo(null);
 
